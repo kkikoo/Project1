8a3b45f07bcc1d39e95dfd8ad72854843cd38933
Shiyi Mu <smu2@uci.edu>
2022-10-17 16:33:02 -0700

    def add_message

--- project1.py
+++ project1.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+code_of_message_type = {1:"CANCELLATION", -1:"CANCELLATION", 2:"ALERT", -2:"ALERT"}
+
+def add_message(time,device_id, message_type, message_name, from_device,):
+    return (time,device_id, message_type, message_name, from_device,)
 
 def _read_input_file_path() -> Path:
     """Reads the input file path from the standard input"""
