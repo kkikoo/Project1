710adf22cba80636e235c65ca38ea3f7880033f8
Shiyi Mu <smu2@uci.edu>
2022-10-17 17:19:43 -0700

    def process_file need more elif & else

--- project1.py
+++ project1.py
@@ -3,7 +3,7 @@ from pathlib import Path
 code_of_message_type = {1:"CANCELLATION", -1:"CANCELLATION", 2:"ALERT", -2:"ALERT"}
 
 def add_message(time, device_id, message_type, message_name, from_device):
-    return (time,device_id, message_type, message_name, from_device)
+    return time,device_id, message_type, message_name, from_device
 
 def my_sort(ls):
     y = sorted(ls, key = lambda ele: (ele[0], ele[1], ele[2], ele[3], ele[4]))
@@ -20,6 +20,26 @@ def print_received(time, device_id, message_type, from_id, message_name):
 def _read_input_file_path() -> Path:
     """Reads the input file path from the standard input"""
     return Path(input())
+
+def process_file(filepath):
+    device = []
+    next_device = {}
+    cost_of_time = {}
+    all_message_name = set()
+    message_list = []
+
+    try:
+        #print filepath
+        with open(filepath, 'r') as f:
+            for line in f.readllines():
+                line - line.strip()
+                if len(line) == 0 or line[0] == '#':
+                    continue
+                word = line.split(' ')
+                #print word
+                if word[0] == 'DEVICE':
+                    device.append(int(word[1]))
+                elif word[0] == "PROPAGATE":
 
 
 def main() -> None:
