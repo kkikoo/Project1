5aa00fb91d63729a738e7b625a7ef94a34369051
Shiyi Mu <smu2@uci.edu>
2022-10-17 22:51:21 -0700

    open file

--- tests/test_project1.py
+++ tests/test_project1.py
@@ -1,3 +1,38 @@
+from project1 import *
+
 import unittest
+
+class demoTestOpen(unittest.TestCase):
+    def __init__(self, filepath):
+        super().__init__()
+        self.filepath = filepath
+    def __enter__(self):
+        self.f = open(self.filepath, 'r+')
+        return self.f
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.f.close()
+        if exc_type != FileNotFoundError:
+            return True
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 
 
