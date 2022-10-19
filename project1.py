4a757cd50210e53db4f4eb1a67f5126aa3d0d19f
Shiyi Mu <smu2@uci.edu>
2022-10-19 00:05:22 -0700

    OKK

--- project1.py
+++ project1.py
@@ -54,94 +54,65 @@ def process_file(filepath):
         print("FILE NOT FOUND")
         pass
 
+def simulation(device, _next_device, cost_time, all_message_name, message_list):
+
+    _from_device = {int(j): int(i) for i, j in _next_device.items()}
+    known = {i: {j: False for j in device} for i in all_message_name}
+
+    while len(message_list) > 0:
+        # run first
+        message_list = my_sort(message_list)
+        first = message_list[0]
+        message_list = message_list[1:]
+
+        time, device_id, message_type, message_name, from_device = first[
+            0], first[1], first[2], first[3], first[4]
+        to_device_id = _next_device[device_id]
+        from_device_id = _from_device[device_id]
+        need_time = cost_time[(device_id, to_device_id)]
+
+        #PRINT FIRST
+        if from_device != -1:  #NOT FROM DEVICE, PRINT FIRST
+            print_received(time, device_id, message_type,
+                           from_device_id, message_name)
+
+        # WHETHER PASS
+        if message_type == 2:  # ALERT
+            if known[message_name][device_id] == False:
+                #ALERT CANCEL
+                #KNOW AND SEND
+                print_sent(time, device_id, message_type,
+                           to_device_id, message_name)
+                _message = add_message(  #RECEIVING
+                    time=time + need_time,
+                    device_id=to_device_id,
+                    message_type=message_type,
+                    message_name=message_name,
+                    from_device=device_id
+                )
+                message_list.append(_message)
+        else:  #CANCELATION
+            if known[message_name][device_id] == False:
+
+                print_sent(time, device_id, message_type,
+                           to_device_id, message_name)
+                _message = add_message(  #THEN RECEIVING
+                    time=time + need_time,
+                    device_id=to_device_id,
+                    message_type=message_type,
+                    message_name=message_name,
+                    from_device=device_id
+                )
+                message_list.append(_message)
+                known[message_name][device_id] = True #NOW, I KNOW
+
 def main():
     """Runs the simulation program in its entirety"""
     input_file_path = _read_input_file_path()
 
     device, next_device, cost_of_time, all_message_name, message_list = process_file(input_file_path)
-    _from_device = {int(j): int(i) for i, j in next_device.items()}
-    known = {i: {j: 0 for j in device} for i in all_message_name}
+    simulation(device, next_device, cost_of_time, all_message_name, message_list)
 
-    while len(message_list) > 0:
-        #print known
-        message_list = my_sort(message_list)
-        first = message_list[0]
-        message_list = message_list[1:]
-        # get specific words
-        time, device_id, message_type, message_name, from_device = first[0], first[1], first[2], first[3], first[4]
-        to_device_id = next_device[device_id]
-        from_device_id = _from_device[device_id]
-        need_time = cost_of_time[(device_id, to_device_id)]
-
-        #start printing
-        if message_type > 0:
-            print_sent(time, device_id, message_type, to_device_id, message_name)
-        else:
-            print_received(time, device_id, message_type, from_device_id, message_name)
-
-        if from_device == -1: #from the device
-            if message_type == 1: #stop sending
-                for _k in known[message_name].keys():
-                    if known[message_name][_k] == 1:
-                        known[message_name][_k] = 2
-                if known[message_name][device_id] == 0:
-                    known[message_name][device_id] = 1
-
-            _message = add_message(
-                time = time + need_time,
-                device_id = to_device_id,
-                message_type = -1 * message_type,
-                message_name = message_name,
-                from_device = device_id
-            )
-            message_list.append(_message)
-
-        elif message_type < 0: #RECEIVED
-            if message_type == -1: #RECEIVED CANCELLATION
-                if known[message_name][device_id] == 2:
-                    continue
-                else:
-                    _message = add_message(
-                        time = time,
-                        device_id = device_id,
-                        message_type = -1 * message_type,
-                        message_name = message_name,
-                        from_device = _from_device[device_id]
-                    )
-                    for _k in known[message_name].keys():
-                        if known[message_name][_k] == 1:
-                            known[message_name][_k] = 2
-                    if known[message_name][device_id] == 0:
-                        known[message_name][device_id] = 1
-
-                    message_list.append(_message)
-            else: #RECEIVED ALERT
-                if known[message_name][device_id] in [1, 2]:
-                    continue
-                else:
-                    _message = add_message(
-                        time = time,
-                        device_id = device_id,
-                        message_type = -1 * message_type,
-                        message_name = message_name,
-                        from_device = _from_device[device_id]
-                    )
-                    message_list.append(_message)
-        else: #STOP SENDING, "CANCELLATION", BEFORE KNOWN, STOP SENDING
-            if message_type == 1 and known[message_name][device_id] == 2:
-                continue
-            #SENDING "ALERT"
-            if message_type == 2 and known[message_name][device_id] in [1, 2]:
-                continue
-            else:
-                _message = add_message(
-                    time = time + need_time,
-                    device_id = to_device_id,
-                    message_type = -1 * message_type,
-                    message_name = message_name,
-                    from_device = device_id
-                )
-                message_list.append(_message)
 
 if __name__ == '__main__':
     main()
