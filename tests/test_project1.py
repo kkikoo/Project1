c4ca937e34dd5a1177295b81842b5678db5e70dd
Shiyi Mu <smu2@uci.edu>
2022-10-18 23:18:50 -0700

    def test_2_messages_arrive_at_the_same_device_at_the_same_time

--- tests/test_project1.py
+++ tests/test_project1.py
@@ -37,26 +37,43 @@ class test_project1(unittest.TestCase):
             ),
                 "@100 #1: SENT CANCELLATION TO #2: TestTrouble-1\n@200 #2: RECEIVED CANCELLATION FROM #1: TestTrouble-2\n")
 
+    def test_2_devices_receive_messages_at_the_same_time(self):
+        # If two devices receive messages at the same time
+        # the device with the [smaller device ID] will process all of its received message(s)
+        # before the device with the larger device ID will process any.
 
+        # Meaning of message_list:(time, device_id, message_type, message_name, from_device,)
+        message_list = [(0, 985, 2, 'Trouble', -1), (0, 211, 2, 'Hello', -1), ]
+        self.assertEqual(my_sort(message_list),
+                         [(0, 211, 2, 'Hello', -1), (0, 985, 2, 'Trouble', -1), ])
 
+    def test_2_messages_arrive_at_the_same_device_at_the_same_time(self):
+        # If two messages arrive at the same device at the same time, they will be processed according to the following priority:
+        # 1. Cancellations are always processed before alerts. (That way, if a device receives an alert and a cancellation of that same alert simultaneously, it won't propagate the alert.)
+        # 2. When two cancellations or two alerts are received simultaneously, their descriptions are compared lexicographically (i.e., using <) and the "lesser" one is processed first.
+        #   So, for example, the alert Boo would be processed before the alert Hello, because 'Boo' < 'Hello' is True.
+        # 3. When two cancellations or two alerts with the same description are received simultaneously, the one sent by the smaller device ID is processed first.
 
+        # Meaning of message_list:(time, device_id, message_type, message_name, from_device,)
+        # Meaning of message_type: 1 means cancel, 2 means album
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # test 1: test if Cancellations always processed before alerts
+        message_list = [(0, 985, 2, 'Trouble', -1), (0, 985, 1, 'Hello', -1), ]
+        self.assertEqual(my_sort(message_list),
+                         [(0, 985, 1, 'Hello', -1), (0, 985, 2, 'Trouble', -1), ])
+        # test 2: test when are both Cancellations or alerts
+        message_list = [(0, 985, 2, 'Hello', -1), (0, 985, 2, 'Boo', -1),
+                        (0, 985, 1, 'Hello', -1), (0, 985, 1, 'Boo', -1), ]
+        self.assertEqual(my_sort(message_list),
+                         [(0, 985, 1, 'Boo', -1), (0, 985, 1, 'Hello', -1),
+                          (0, 985, 2, 'Boo', -1), (0, 985, 2, 'Hello', -1), ]
+                         )
+        # test 3: test when are both Cancellations or alerts with same description
+        message_list = [(0, 985, 1, 'Trouble', 1),
+                        (0, 985, 1, 'Trouble', -1), ]
+        self.assertEqual(my_sort(message_list),
+                         [(0, 985, 1, 'Trouble', -1), (0, 985, 1, 'Trouble', 1), ]
+                         )
 
 
 if __name__ == '__main__':
