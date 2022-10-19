3946d6fdc900433ea11a865b272cced129637f74
Shiyi Mu <smu2@uci.edu>
2022-10-18 21:04:13 -0700

    def test_file_output

--- tests/test_project1.py
+++ tests/test_project1.py
@@ -12,6 +12,16 @@ class test_project1(unittest.TestCase):
             self.assertEqual(output.getattribute(), "FILE NOT FOUND\n")
 
     def test_file_output(self):
+        filepath = Path("sample_input.txt")
+        device, _next_device, cost_time, all_message_name, message_list = process_file(
+            filepath)
+        self.assertEqual(device, [1, 2, 3, 4])
+        self.assertEqual(_next_device, {1: 2, 2: 3, 3: 4, 4: 1})
+        self.assertEqual(
+            cost_time, {(1, 2): 750, (2, 3): 1250, (3, 4): 500, (4, 1): 1000})
+        self.assertEqual(all_message_name, {'Trouble'})
+        self.assertEqual(
+            message_list, [(0, 1, 2, 'Trouble', -1), (2200, 1, 1, 'Trouble', -1)])
 
 
 
